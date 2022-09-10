9c1f8558abdd80b7749de9b3a6bf974947316ba1
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-09-11 00:38:19 +0300

    Add files via upload

---
+++ src/Timer.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Timer() {
+  const [time, setTime] = useState(0);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  }
+
+  return (
+    <>
+      <h1>Time: {time}</h1>
+      <input value={name} onChange={handleChange} type='text' />
+    </>
+  );
+}
+
